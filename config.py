32928f7e97712e780b8356ef914e788cd4c09e7e
Ubuntu <ubuntu@ip-172-31-0-246.ec2.internal>
2023-03-05 09:39:22 +0000

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cek64lku9ctq.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "mypassword"
+customdb = "employee"
+custombucket = "bucket-for-project-sunilbennur"
+customregion = "us-east-1"
